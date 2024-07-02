a19902b502f15ad6159fe681cd9e3e36da65f4a1
Eric Friday <ericfriday@gmail.com>
2024-07-02 05:33:51 -0500

    up

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import 'assets/clear.css';
-import 'assets/font.css';
+import './assets/clear.css';
+import './assets/font.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
